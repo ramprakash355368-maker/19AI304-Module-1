0af8c3d3530b399163beff3ef85209cf45bd550e
Kumaravel S <skumaransathya2006@gmail.com>
2026-05-31 19:04:00 +0530

    Add C program to check if number is zero
    Implement a C program to read an integer and check if it is equal to 0.

---
+++ Write a C program to read X value.c
@@ -0,0 +1,13 @@
+// Write a C program to read X value and check whether that number is equal to 000 using an if-else?
+
+#include<stdio.h>
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    if(num==000)
+    printf("Number is equal to 000");
+    else
+    printf("Number is NOT equal to 000");
+    return 0;
+}
