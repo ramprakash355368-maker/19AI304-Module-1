ace8711b68d9eab3f1839fd669474858c29e7d7a
Kumaravel S <skumaransathya2006@gmail.com>
2026-05-31 19:03:34 +0530

    Implement simple interest calculation in C
    This program calculates simple interest based on user inputs for principal, rate, and time.

--- Write a C program to find simple interest.c
+++ Write a C program to find simple interest.c
@@ -1,1 +1,10 @@
+//Write a C program to find simple interest.  Note: Inputs are principle, year and rate.
 
+#include <stdio.h>
+int main()
+{
+    float num1,num2,num3;
+    scanf("%f%f%f",&num1,&num2,&num3);
+    printf("Simple Interest = %.2f",(num1*num2*num3)/100);
+    return 0;
+}
