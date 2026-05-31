33513ca77571a0859c81360ee75622c58dcaf3c4
Kumaravel S <skumaransathya2006@gmail.com>
2026-05-31 19:02:49 +0530

    Create Write a C program to find the ASCII.c

---
+++ Write a C program to find the ASCII.c
@@ -0,0 +1,10 @@
+//Write a C program to find the ASCII character of the given value.
+
+#include <stdio.h>
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    printf("Character of ASCII Value %d is %c",num,num);
+    return 0;
+}
